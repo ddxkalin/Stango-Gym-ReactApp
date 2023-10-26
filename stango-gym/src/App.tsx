71e74fb3a5f20db6ed0a665b1ac3fc9d68fd5d8a
Kalin Stoev <kstoev9316@gmail.com>
2023-10-26 14:29:27 +0100

    NavBar Done

--- stango-gym/src/App.tsx
+++ stango-gym/src/App.tsx
@@ -1,8 +1,30 @@
 import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import {SelectedPage} from "@/shared/types"
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => removeEventListener("scroll", handleScroll);
+  }, []);
+
   return <div className="app bg-gray-20">
-    <Navbar />
+    <Navbar 
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+    />
   </div>;
 }
 
